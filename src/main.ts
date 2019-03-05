636100489ba4f3de6c9e9eb1bacb0008842ac022
Emmanuel Oppong-Sarpong <esarpong51@gmail.com>
2019-03-05 08:46:02 +0000

    Initial Commit

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import Vue from "vue";
+import './plugins/vuetify'
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./registerServiceWorker";
+
+import firebase from 'firebase';
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDjSnIXylcpa-3WXr7g5Q-YjHSbJ1aiOHw",
+    authDomain: "room-63.firebaseapp.com",
+    databaseURL: "https://room-63.firebaseio.com",
+    projectId: "room-63",
+    storageBucket: "room-63.appspot.com",
+    messagingSenderId: "37014886737"
+};
+firebase.initializeApp(config);
 
 Vue.config.productionTip = false;
 
@@ -11,3 +25,5 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount("#app");
+
+
